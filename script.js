86f9341420a034c13661c8837bb4e8fea261a256
airp0 <103009672+aipr0@users.noreply.github.com>
2023-03-05 17:09:47 +0000

    Finalise main functionality of sketcher, need to add limit on input <= 100

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 //Setting intial size of the sketcher
 let gridCount = 16;
 
-function drawGrid() {
+function createGrid() {
     const sketcherGrid = document.querySelector('.sketcher');
+    //Wipes the grid every time createGrid is run, to append new divs to draw the new grid
+    sketcherGrid.innerHTML = '';
 
     //For use in style.css, allows grid-template-rows and grid-template-columns to share space equally between the divs
     sketcherGrid.style.setProperty('--grid-number', gridCount);
@@ -17,12 +19,25 @@ function drawGrid() {
     }
 }
 
-drawGrid();
+function drawOnGrid() {
+    let gridCells = document.querySelectorAll('.pinkbox');
 
-let gridCells = document.querySelectorAll('.pinkbox');
-
-gridCells.forEach((cell) => {
-    cell.addEventListener('mouseover', function(e) {
-        this.style.backgroundColor = 'black';
+    gridCells.forEach((cell) => {
+        cell.addEventListener('mouseover', function(e) {
+            this.style.backgroundColor = 'black';
+        })
     })
-})
+}
+
+//Initialises the sketcher
+createGrid();
+drawOnGrid();
+
+//Grabs the button, attaches a prompt and the value becomes gridCount for use in createGrid();
+const userDeterminedGridCount = document.getElementById('setGridSize');
+userDeterminedGridCount.addEventListener('click', function() {
+   gridCount = prompt("Enter a number to determine grid size:", 16);
+   createGrid();
+   drawOnGrid();
+});
+
