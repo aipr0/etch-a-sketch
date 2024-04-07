1f633edd471a42ea781762edf0e147440f59d9c5
rinzler <aipr0@protonmail.com>
2024-04-07 19:08:06 +0100

    Create drawGrid() function

---
+++ script.js
@@ -0,0 +1,28 @@
+const gridSizer = document.querySelector(".sizer-button");
+
+gridSizer.addEventListener("click", () => {
+    let gridSize = prompt("Enter a value to determine grid size:", "16");
+    drawGrid(gridSize);
+});
+
+// I'm really proud of how simple this is
+function drawGrid(gridSize) {
+    const container = document.querySelector(".grid");
+
+    for(i = 0; i < gridSize; i++) {
+        const gridRow = document.createElement("div");
+        gridRow.classList.add("grid-row");
+
+        for(j = 0; j < gridSize; j++) {
+            const gridSquare = document.createElement("div");
+            gridSquare.classList.add("grid-square");
+
+            gridRow.appendChild(gridSquare);
+        }
+        
+        container.appendChild(gridRow);
+    }
+    
+}
+
+drawGrid(16);
