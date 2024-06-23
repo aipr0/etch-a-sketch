60cb320e15ebdb7dd728c0fc8d65b4dcabc1230f
aipr0 <103009672+aipr0@users.noreply.github.com>
2024-06-23 14:14:57 +0100

    Adjust indent size from 4 spaces to 2

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
 const gridSizer = document.querySelector(".sizer-button");
 
 gridSizer.addEventListener("click", () => {
-    let gridSize = prompt("Enter a value to determine grid size:", "16");
-    drawGrid(gridSize);
+  let gridSize = prompt("Enter a value to determine grid size:", "16");
+  drawGrid(gridSize);
 });
 
 // I'm really proud of how simple this is
 function drawGrid(gridSize) {
-    const container = document.querySelector(".grid");
+const container = document.querySelector(".grid");
 
-    container.innerHTML = "";
+container.innerHTML = "";
 
-    for(i = 0; i < gridSize; i++) {
-        const gridRow = document.createElement("div");
-        gridRow.classList.add("grid-row");
+for(i = 0; i < gridSize; i++) {
+  const gridRow = document.createElement("div");
+  gridRow.classList.add("grid-row");
 
-        for(j = 0; j < gridSize; j++) {
-            const gridSquare = document.createElement("div");
-            gridSquare.classList.add("grid-square");
+  for(j = 0; j < gridSize; j++) {
+    const gridSquare = document.createElement("div");
+    gridSquare.classList.add("grid-square");
 
-            gridSquare.addEventListener("mouseover", () => {
-                gridSquare.classList.add("grid-square-selected");
-            })
+    gridSquare.addEventListener("mouseover", () => {
+        gridSquare.classList.add("grid-square-selected");
+    })
 
-            gridRow.appendChild(gridSquare);
-        }
-        
-        container.appendChild(gridRow);
-    }
-    
+    gridRow.appendChild(gridSquare);
+  }
+  
+  container.appendChild(gridRow);
+}
+
 }
 
 drawGrid(16);
